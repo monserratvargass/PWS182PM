0264784f784cf4b07617c09d9c03074503e0698b
monserratvargass <guvamonse@gmail.com>
2023-11-02 22:33:04 -0600

    Propuesta de interfaces para los formularios

---
+++ BeastMex/resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
+    
+    {{-- @vite(['resources/js/app.js']) --}}
+
+    <title>@yield('titulo')</title>
+</head>
+<body>
+    
+    @include('partials.navbar')
+
+    @yield('contenido')
+
+</body>
+</html>
--- BeastMex/routes/web.php
+++ BeastMex/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+ Route::get('/', function () {
     return view('welcome');
+}); 
+
+Route::get('/gerenciaRegistroUsuario', function () {
+    return view('gerenciaRegistroUsuario');
 });
+
