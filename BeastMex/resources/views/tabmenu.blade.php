23d5040a262166241fbb850fba40f1e71a977ab5
zxamu <jsszmdpnc@outlook.com>
2023-11-05 15:27:07 -0600

    actualizacion gerencia

--- BeastMex/resources/views/tabmenu.blade.php
+++ BeastMex/resources/views/tabmenu.blade.php
@@ -1,57 +1,234 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 <div class="container">
-    <div class="d-block d-sm-none">
-      <label for="Tab" class="sr-only">Tab</label>
+  <div class="d-block d-sm-none">
+    <label for="Tab" class="sr-only">Tab</label>
   
-      <select id="Tab" class="form-select w-100 rounded border border-gray-200">
-        <option>Settings</option>
-        <option>Messages</option>
-        <option>Archive</option>
-        <option selected>Notifications</option>
-      </select>
-    </div>
+    <select id="Tab" class="form-select w-100 rounded border border-gray-200">
+      <option value="settings">Usuarios</option>
+      <option value="messages">Registro</option>
+    </select>
+  </div>
   
-    <div class="d-none d-sm-block">
-      <div class="border-bottom border-gray-200">
-        <ul class="nav nav-tabs gap-3">
-          <li class="nav-item">
-            <a
-              href="/gerenciaCRUD"
-              class="nav-link p-3 text-sm font-weight-medium text-muted text-decoration-none"
-            >
-              Settings
-            </a>
-          </li>
+  <div class="d-none d-sm-block">
+    <div class="border-bottom border-gray-200">
+      <ul class="nav nav-tabs gap-3">
+        <li class="nav-item">
+          <a href="#settings" class="nav-link p-3 text-sm font-weight-medium text-muted text-decoration-none">Usuarios</a>
+        </li>
   
-          <li class="nav-item">
-            <a
-              href="#"
-              class="nav-link p-3 text-sm font-weight-medium text-muted text-decoration-none"
-            >
-              Messages
-            </a>
-          </li>
-  
-          <li class="nav-item">
-            <a
-              href="#"
-              class="nav-link p-3 text-sm font-weight-medium text-muted text-decoration-none"
-            >
-              Archive
-            </a>
-          </li>
-  
-          <li class="nav-item">
-            <a
-              href="#"
-              class="nav-link rounded-top border border-gray-300 border-bottom-0 p-3 text-sm font-weight-medium text-primary"
-            >
-              Notifications
-            </a>
-          </li>
-        </ul>
-      </div>
+        <li class="nav-item">
+          <a href="#messages" class="nav-link p-3 text-sm font-weight-medium text-muted text-decoration-none">Registro</a>
+        </li>
+      </ul>
     </div>
   </div>
   
+  <div id="settings" class="tab-content">
+    <div class="container">
+      <div class="table-responsive">
+          <table class="table table-bordered table-striped">
+            <thead>
+              <tr>
+                <th class="px-4 py-2 font-weight-bold text-dark">
+                  Nombre
+                </th>
+                <th class="px-4 py-2 font-weight-bold text-dark">
+                  E-mail
+                </th>
+                <th class="px-4 py-2 font-weight-bold text-dark">
+                  Tipo
+                </th>
+                <th class="px-4 py-2 font-weight-bold text-dark">
+                  Salario
+                </th>
+                <th class="px-4 py-2">Acciones</th>
+              </tr>
+            </thead>
+        
+            <tbody>
+              <tr>
+                <td class="px-4 py-2 font-weight-bold text-dark">
+                  John Doe
+                </td>
+                <td class="px-4 py-2 text-muted">24/05/1995</td>
+                <td class="px-4 py-2 text-muted">Web Developer</td>
+                <td class="px-4 py-2 text-muted">$120,000</td>
+                <td class="px-4 py-2">
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Editar
+                  </a>
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Eliminar
+                  </a>
+                </td>
+              </tr>
+        
+              <tr>
+                <td class="px-4 py-2 font-weight-bold text-dark">
+                  Jane Doe
+                </td>
+                <td class="px-4 py-2 text-muted">04/11/1980</td>
+                <td class="px-4 py-2 text-muted">Web Designer</td>
+                <td class="px-4 py-2 text-muted">$100,000</td>
+                <td class="px-4 py-2">
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Editar
+                  </a>
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Eliminar
+                  </a>
+                </td>
+              </tr>
+        
+              <tr>
+                <td class="px-4 py-2 font-weight-bold text-dark">
+                  Gary Barlow
+                </td>
+                <td class="px-4 py-2 text-muted">24/05/1995</td>
+                <td class="px-4 py-2 text-muted">Singer</td>
+                <td class="px-4 py-2 text-muted">$20,000</td>
+                <td class="px-4 py-2">
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Editar
+                  </a>
+                  <a
+                    href="#"
+                    class="btn btn-primary btn-sm"
+                  >
+                    Eliminar
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+  </div>
+  
+  <div id="messages" class="tab-content">
+      <div class="container text-center">
+              <form action="" class="mb-4">
+                <div class="mb-3">
+                  <label for="name" class="form-label">Nombre</label>
+                  <input
+                    class="form-control"
+                    type="text"
+                    id="name"
+                    placeholder="Nombre"
+                  />
+                </div>
+    
+                <div class="row">
+                  <div class="col">
+                    <label for="email" class="form-label">Email</label>
+                    <input
+                      class="form-control"
+                      type="email"
+                      id="email"
+                      placeholder="Direccion de Email"
+                    />
+                  </div>
+    
+                  <div class="col">
+                    <label for="salary" class="form-label">Salario</label>
+                    <input
+                      class="form-control"
+                      type="tel"
+                      id="salary"
+                      placeholder="Salario"
+                    />
+                  </div>
+                </div>
+    
+                <div class="mt-3">
+                  <div class="form-check form-check-inline">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      id="option1"
+                      name="option"
+                    />
+                    <label
+                      class="form-check-label"
+                      for="option1"
+                    >
+                      Empleado
+                    </label>
+                  </div>
+    
+                  <div class="form-check form-check-inline">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      id="option2"
+                      name="option"
+                    />
+                    <label
+                      class="form-check-label"
+                      for="option2"
+                    >
+                      Administrador
+                    </label>
+                  </div>
+    
+                  <div class="form-check form-check-inline">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      id="option3"
+                      name="option"
+                    />
+                    <label
+                      class="form-check-label"
+                      for="option3"
+                    >
+                      Cliente
+                    </label>
+                  </div>
+                </div>
+
+                <div class="mt-3">
+                  <button
+                    type="submit"
+                    class="btn btn-dark"
+                  >
+                    Guardar
+                  </button>
+                </div>
+              </form>
+  </div>
+  
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+    // Por defecto, muestra la primera vista (Settings) y oculta las demás
+    $("#settings").show();
+    $("#messages, #archive, #notifications").hide();
+
+    // Maneja el cambio de vista cuando se selecciona una pestaña
+    $(".nav-link").click(function(e) {
+      e.preventDefault();
+      var tabId = $(this).attr("href").substring(1); // Obtén el ID de la vista
+      $(".tab-content").hide(); // Oculta todas las vistas
+      $("#" + tabId).show(); // Muestra la vista correspondiente
+    });
+  });
+</script>
+
