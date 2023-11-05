bfa9bd4991b2e8d71773c9f76153fce49fdaff60
121038217 <125855991+121038217@users.noreply.github.com>
2023-11-05 08:57:34 -0600

    paquetes

--- BeastMex/vite.config.js
+++ BeastMex/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -10,13 +9,6 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
-            //importamos Bootstrap 5
-            resolve:{
-                alias:{
-                    '~bootstrap':path.resolve(__dirname, 'node_module/bootstrap'),
-                    }
-            },
-            // fin importamos Bootstrap 5
         }),
     ],
 });
